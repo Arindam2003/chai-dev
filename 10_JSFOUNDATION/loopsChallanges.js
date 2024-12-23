92d4b4b4beb2d7b7fe205fdd043eb1cea0291e3b
Arindam2003 <dindaarindam000@gmail.com>
2024-12-23 10:50:43 +0530

    10_JS_Foundation_Array_Loops

---
+++ 10_JSFOUNDATION/loopsChallanges.js
@@ -0,0 +1,23 @@
+//! 1. While loop calculate the sum of all number formm 1 to 5 and store the result is a variable named 'sum'
+
+let sum=0;
+n=1;
+while(n<=5)
+{
+    sum=sum + n;
+    n++;
+}
+//? console.log(sum);
+
+//! 2. store array 'Countdown' of a number 5 to 1 using while loop.
+
+let Countdown=[]
+let j =5;
+while(j>=1){
+    Countdown.push(j);
+    j--;
+}
+//? console.log(Countdown);
+
+
+
